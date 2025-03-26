5a779245a18975c740bddc4d26e80bfd69935dba
stormozov <web@stormozov.ru>
2025-03-26 19:31:52 +0300

    Реализовал первоначальную версию получения фото из ВК

--- assets/js/api/VK.js
+++ assets/js/api/VK.js
@@ -6,17 +6,72 @@
  * */
 class VK {
 
-  static ACCESS_TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008';
+  static PATH = 'https://api.vk.com/method/';
+  static ACCESS_TOKEN = StorageManager.getCookie('vkid_token');
+  static API_VERSION = '5.199';
   static lastCallback;
+  static lastRequestUrl;
 
   /**
    * Получает изображения
    * */
-  static get(id = '', callback) {}
+  static get(id = '', callback) {
+    this.setLastCallback(callback.name);
+
+    const params = {
+      owner_id: id,
+      album_id: 'profile',
+      access_token: this.ACCESS_TOKEN,
+      v: this.API_VERSION,
+      callback: this.lastCallback
+    };
+
+    const queryString = new URLSearchParams(params).toString();
+    this.lastRequestUrl = `${this.PATH}photos.get?${queryString}`;
+    this.createScript(this.lastRequestUrl);
+  }
 
   /**
    * Передаётся в запрос VK API для обработки ответа.
    * Является обработчиком ответа от сервера.
    */
-  static processData(result) {}
+  static processData(result) {
+    if (result && result.response) {
+      const data = result;
+      console.log(data)
+    } else {
+      console.error(result.error);
+      alert(result.error.error_msg);
+    }
+
+    this.removeScript(this.lastRequestUrl);
+    this.lastRequestUrl = null;
+  }
+
+  /**
+   * Сохраняет последнюю функцию обратного вызова в свойстве lastCallback
+   */
+  static setLastCallback(callback) {
+    this.lastCallback = callback;
+    window[callback] = this.processData.bind(this);
+  }
+
+  /**
+   * Добавляет скрипт в документ для отправки API запроса к VK
+   */
+  static createScript(url) {
+    const script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
+  }
+
+  static removeScript(url) {
+    const scripts = document.querySelectorAll('script');
+    for (let script of scripts) {
+      if (script.src === url) {
+        document.body.removeChild(script);
+        break;
+      }
+    }
+  }
 }
--- assets/js/ui/SearchBlock.js
+++ assets/js/ui/SearchBlock.js
@@ -3,12 +3,28 @@
  * Используется для взаимодействием со строкой ввода и поиска изображений
  * */
 class SearchBlock {
-  constructor(element) {}
+  constructor(element) {
+    this.element = element;
+    this.userInput = this.element.querySelector('input');
+    this.addButton = this.element.querySelector('.add');
+
+    // Подписываемся на события
+    this.registerEvents();
+  }
 
   /**
    * Выполняет подписку на кнопки "Заменить" и "Добавить"
    * Клик по кнопкам выполняет запрос на получение изображений и отрисовывает их,
    * только клик по кнопке "Заменить" перед отрисовкой очищает все отрисованные ранее изображения
    */
-  registerEvents() {}
+  registerEvents() {
+    this.addPhotoButtonHandler();
+  }
+
+  addPhotoButtonHandler() {
+    this.addButton.addEventListener('click', () => {
+      const query = this.userInput.value.trim();
+      if (query) VK.get(query, new ImageViewer().drawImages);
+    });
+  }
 }
