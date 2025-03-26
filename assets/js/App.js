cbf899b14488db45ba732beee4cf094e09bafaaf
stormozov <web@stormozov.ru>
2025-03-26 11:28:44 +0300

    Добавляю модальное окно авторизации в ВК
    Данная модалка нужна для авторизации пользователя в ВК для получения токена доступа. Авторизация реализована через "Кнопка One Tap"

--- assets/js/App.js
+++ assets/js/App.js
@@ -13,6 +13,7 @@ class App {
     this.imageViewer = new ImageViewer(document.getElementsByClassName('images-wrapper')[0]);
     
     this.initModals();
+    this.initVKAuth();
   }
 
   /**
@@ -33,5 +34,39 @@ class App {
    * */
   static getModal(name) {
     return this.modals[name];
+  }
+
+  /**
+   * Инициализирует всплывающее окно авторизации
+   */
+  static initVKAuth() {
+    const modal = document.querySelector('.vk-auth-modal');
+    if (!modal) return;
+
+    const updateModalVisibility = () => {
+      const hasToken = this.getCookie('vkid_token');
+      modal.classList.toggle('hidden', !!hasToken);
+    };
+
+    updateModalVisibility();
+    setInterval(updateModalVisibility, 1000);
+  }
+
+  /**
+   * Возвращает значение куки по ее названию.
+   * @param {string} name Название куки
+   * @returns {string|null} Значение куки или null, если куки не существует
+   */
+  static getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+
+    return null;
   }
 }
