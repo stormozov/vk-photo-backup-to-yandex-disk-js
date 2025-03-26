e80a23660da2177b5518efd9d2d3f3975c885253
stormozov <web@stormozov.ru>
2025-03-26 13:58:07 +0300

    Улучшил метод getCookie
    Улучшил процесс извлечения куки, используя Regex

--- assets/js/utils/StorageManager.js
+++ assets/js/utils/StorageManager.js
@@ -24,16 +24,17 @@ class StorageManager {
    * @returns {string|null} Значение куки или null, если куки не существует
    */
   static getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    if (!name) {
+      console.warn('Метод StorageManager.getCookie() вызван без имени куки.');
+      return undefined;
     }
 
-    return null;
+    const escapedName = name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1');
+    const regex = new RegExp('(?:^|; )' + escapedName + '=([^;]*)');
+
+    const matches = document.cookie.match(regex);
+
+    return matches ? decodeURIComponent(matches[1]) : undefined;
   }
 
   /**
