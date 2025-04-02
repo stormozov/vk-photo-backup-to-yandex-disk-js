309b73fb925dad3536f1195b7b82b1915eb64a74
stormozov <web@stormozov.ru>
2025-04-02 18:59:23 +0300

    Добавляю обработку для кнопки "Добавить"
    Добавляю обработку клика по кнопке "Добавить"  и дополнительно улучшаю класс SearchBlock

--- assets/js/ui/SearchBlock.js
+++ assets/js/ui/SearchBlock.js
@@ -4,6 +4,12 @@
  * */
 class SearchBlock {
 
+  #userInput;
+  #addButton;
+  #replaceButton;
+  #displayedImages;
+  #imageViewer;
+
   /**
    * @param {HTMLElement} searchContainer - DOM-элемент, в котором находится строка ввода 
    * и кнопка поиска
@@ -11,14 +17,18 @@ class SearchBlock {
    */
   constructor(searchContainer, imageViewer) {
     // Получаем DOM-элементы
-    this.userInput = searchContainer.querySelector('input');
-    this.addButton = searchContainer.querySelector('.add');
+    this.#userInput = searchContainer.querySelector('input');
+    this.#addButton = searchContainer.querySelector('.add');
+    this.#replaceButton = searchContainer.querySelector('.replace');
+    
+    // Коллекция для хранения уникальных изображений
+    this.#displayedImages = new Set();
 
     // Сохраняем экземпляр ImageViewer
-    this.imageViewer = imageViewer;
+    this.#imageViewer = imageViewer;
 
     // Подписываемся на события
-    this.registerEvents();
+    this.#registerEvents();
   }
 
   /**
@@ -27,22 +37,71 @@ class SearchBlock {
    * только клик по кнопке "Заменить" перед отрисовкой очищает все отрисованные 
    * ранее изображения
    */
-  registerEvents() {
-    this.addPhotoButtonHandler();
+  #registerEvents() {
+    this.#addPhotoButtonHandler();
+    this.#replacePhotoButtonHandler();
   }
 
-  addPhotoButtonHandler() {
-    this.addButton.addEventListener('click', () => {
-      const query = this.userInput.value.trim();
-      if (query) {
-        if (/^\d+$/.test(query)) {
-          const userId = parseInt(query, 10);
-          VK.get(userId, (images) => this.imageViewer.drawImages(images));
-        } else {
-          this.userInput.value = '';
-          alert('Идентификатор должен быть целым числом (например: 123456).');
-        }
-      }
+  /**
+   * Добавляет обработчик клика по кнопке "Добавить"
+   * При клике выполняет запрос на получение изображений
+   */
+  #addPhotoButtonHandler() {
+    this.#addButton.addEventListener('click', () => {
+      this.#handleUserInput(this.#addImages.bind(this));
     });
+  }
+
+  /**
+   * Добавляет обработчик клика по кнопке "Заменить"
+   * При клике выполняет запрос на получение изображений
+   */
+  #replacePhotoButtonHandler() {
+    this.#replaceButton.addEventListener('click', () => {
+      this.#handleUserInput(this.#replaceImages.bind(this));
+    });
+  }
+
+  /**
+   * Обрабатывает ввод пользователя
+   * @param {Function} callback - функция обратного вызова
+   */
+  #handleUserInput(callback) {
+    const query = this.#userInput.value.trim();
+    if (query) {
+      if (/^\d+$/.test(query)) {
+        const userId = parseInt(query, 10);
+        VK.get(userId, (images) => callback(images));
+      } else {
+        this.#userInput.value = '';
+        alert('Идентификатор должен быть целым числом (например: 123456).');
+      }
+    }
+  }
+
+  /**
+   * Добавляет новые уникальные изображения
+   * @param {Array} images - массив изображений
+   */
+  #addImages(images) {
+    const newImages = images.filter((image) => !this.#displayedImages.has(image.id));
+
+    if (newImages.length > 0) {
+      newImages.forEach((image) => this.#displayedImages.add(image.id));
+      this.#imageViewer.drawImages(newImages);
+    } else {
+      alert('Нет новых изображений для отображения.');
+    }
+  }
+
+  /**
+   * Заменяет все изображения
+   * @param {Array} images - массив изображений
+   */
+  #replaceImages(images) {
+    this.#displayedImages.clear();
+    this.#imageViewer.clear();
+    images.forEach((image) => this.#displayedImages.add(image.id));
+    this.#imageViewer.drawImages(images);
   }
 }
